50ee56727a61b1828320584c43fd97add3847162
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-10-06 10:52:33 +0100

    Templates are building off dummy data

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,43 @@
-const {src, dest, watch, series, parallel} = require('gulp');
-// const fs = require('fs');
-// const gulp = require('gulp');
-// const posthtml = require('gulp-posthtml');
-const mjml = require('gulp-mjml');
-
-
-
 // Gulp requires
+const { src, dest, watch, series, parallel } = require('gulp');
 const plugins = require('gulp-load-plugins')();
-const liquid = require('@tuanpham-dev/gulp-liquidjs')
+const mjml = require('gulp-mjml');
+const liquid = require('@tuanpham-dev/gulp-liquidjs');
+const mjmlEngine = require('mjml');
+// Other requires
+const fs = require('fs');
+const path = require('path');
 
-// Local requires
-const emailDummyData = require('./src/templates/welcome.json');
-console.log(emailDummyData);
+// Handle errors for MJML issues
+function handleMJMLErrors(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
 
 const buildEmails = () => {
   return src('src/templates/**/*.mjml')
-    .pipe(plugins.data((file) => emailDummyData))
-    .pipe(liquid({
-      ext: '.mjml'
-    }))
-    .pipe(mjml())
-
-    // .pipe(plugins.formatHtml())
-    // .pipe(plugins.htmlmin({ removeComments: true, collapseWhitespace: true }))
+    .pipe(
+      plugins.data(file => {
+        let dummyData;
+        try {
+          dummyData = JSON.parse(
+            fs.readFileSync(path.dirname(file.path) + '/' + path.parse(file.path).name + '.dummyData.json')
+          );
+        } catch (error) {
+          dummyData = null;
+        }
+        return dummyData;
+      })
+    )
+    .pipe(
+      liquid({
+        ext: '.mjml'
+      })
+    )
+    .pipe(mjml(mjmlEngine, { beautify: true, validation: 'strict' }))
+    .on('error', handleMJMLErrors)
     .pipe(dest('builded/'));
 };
-
-
-// gulp.task('html', function () {
-//   var plugins = [
-//     require('posthtml-lorem')(),
-//   ];
-
-//   fs.readdir('./languages/', function (err, files) {
-//     if (err) throw err;
-//     files.forEach(function (file) {
-//       var language = file.split('.')[0];
-//       // Gets .html and .nunjucks files in pages
-//       return gulp.src('./pages/**/*.+(html|nunjucks)')
-//         // Adding data to Nunjucks
-//         .pipe(data(function () {
-//           return require('./languages/' + language + '.json')
-//         }))
-//         // Renders template with nunjucks
-//         .pipe(nunjucksRender({
-//           path: ['./helpers/']
-//         }))
-//         .pipe(posthtml(plugins))
-//         .pipe(mjml())
-//         // output files in app folder
-//         .pipe(gulp.dest('./dest/' + language));
-//     });
-//   });
-// });
 
 // exports.default = serve;
 exports.buildEmails = buildEmails;
