36f20f2452fceb8601f96e7caf2d9f82d81a83d0
Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
2020-10-08 15:02:37 +0100

    Ds/bugs (#11)
    * Updated links to new disruptions page. Fixes #1
    
    * Updated includes to liquid so we can render vars within them
    
    * Updated readme docs
    
    * Updated build process so vars get injected in the correct place
    
    * Updated signUp to use MJML includes
    
    * Updated readme include docs

--- gulp-tasks/buildMJMLWithDummyData.js
+++ gulp-tasks/buildMJMLWithDummyData.js
@@ -17,8 +17,6 @@ function handleMJMLErrors(err) {
 const buildMJMLWithDummyData = () => {
   return (
     src(paths.templates.src)
-      // Render MJML templates
-      .pipe(plugins.mjml(mjmlEngine, { beautify: true, validation: 'strict' }))
       // Inject dummy data
       .pipe(
         plugins.data(file => {
@@ -38,10 +36,18 @@ const buildMJMLWithDummyData = () => {
       // Render the variables/logic in the templates
       .pipe(
         liquid({
+          ext: '.mjml'
+        })
+      )
+      // Render MJML templates
+      .pipe(plugins.mjml(mjmlEngine, { beautify: true, validation: 'strict' }))
+      .on('error', handleMJMLErrors)
+      // Render the variables/logic from partials pulled in from mjml include
+      .pipe(
+        liquid({
           ext: '.html'
         })
       )
-      .on('error', handleMJMLErrors)
       .pipe(dest(paths.templates.output))
       // After html templates are created, generate some txt ones...
       .pipe(plugins.html2txt({ ignoreImage: true }))
